7019fde173fc5bc3d19b56ee89af6280ba06257e
Henry-ai443 <henrymaina2024@outlook.com>
2025-10-24 06:31:54 -0400

    Admin dashboard styling

--- src/components/Admin/Quickstats.jsx
+++ src/components/Admin/Quickstats.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FaCalendarAlt, FaBullhorn, FaUsers } from "react-icons/fa";
-import './admin.css'
 
 const QuickStats = () => {
   const [stats, setStats] = useState({
